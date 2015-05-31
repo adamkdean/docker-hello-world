49b04767e69f8d404006a8f4932e66be15310752
Adam K Dean <adam@amberfish.co.uk>
2015-05-31 13:57:47 +0100

    Added instance naming

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var koa = require('koa'),
     logger = require('koa-logger'),
+    funkyname = require('funkyname.js')
     port = process.env.PORT || 8000,
-    app = koa();
+    name = funkyname.getFunkyName(),
+    app = koa()
 
 app.use(logger());
 
 app.use(function *(){
-    this.body = 'Hello World -- Update 1';
+    this.body = 'This instance is called ' + name;
 });
 
 app.listen(port, function () {
